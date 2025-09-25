c4c6ecdb6038238fdfca7be7134d8afee909f74f
pje8740 <pje8740@cu.ac.kr>
2025-09-25 09:39:10 +0900

    수정 완료3

--- src/App.jsx
+++ src/App.jsx
@@ -67,6 +67,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="home" element={<HomePage />} />
           <Route index element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
           <Route path="contact" element={<ContactPage />} />
